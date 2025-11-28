1b40e5ab10ac12ebf50e9c4fc5edcbfacf217ac3
bob <bobvanderstaak@hotmail.com>
2025-11-28 12:41:43 +0100

    fixed handler to set other heather response and varname to false

--- handler.go
+++ handler.go
@@ -364,16 +364,19 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyht
 			zap.String("host", r.Host),
 		)
 		r.Header.Set(h.RequestHeader, "false")
+		r.Header.Set(h.ResponseHeader, "false")
+		caddyhttp.SetVar(r.Context(), h.VarName, "false")
+
 	}
 	if h.RequestHeader != "" && !isBlocked {
 		r.Header.Set(h.RequestHeader, "true")
 	}
 
-	if h.ResponseHeader != "" {
+	if h.ResponseHeader != "" && !isBlocked {
 		w.Header().Set(h.ResponseHeader, "true")
 	}
 
-	if h.VarName != "" {
+	if h.VarName != "" && !isBlocked {
 		caddyhttp.SetVar(r.Context(), h.VarName, "true")
 	}
 
